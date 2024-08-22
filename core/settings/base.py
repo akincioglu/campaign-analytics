e657f338f65b07f23b4afedcbe6bf96b10d4bbeb
akincioglu <akincioglu23@gmail.com>
2024-08-22 03:41:55 +0300

    chore: Updated directory structure and module loading configuration

--- core/settings/base.py
+++ core/settings/base.py
@@ -1,14 +1,38 @@
 from pathlib import Path
 
 # Base settings for both environments
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 import os
 from dotenv import load_dotenv
 
+dotenv_path = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_path):
+    load_dotenv(dotenv_path=dotenv_path)
+else:
+    print(f".env file not found at {dotenv_path}")
 
-SECRET_KEY = "g$hkBq>=B$fzOYV"
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv("SECRET_KEY", "g$hkBq>=B$fzOYV")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+print(f"LOADED API_VERSION: {os.getenv('API_VERSION')}")
+
+
+def get_modules_for_version(api_version):
+    modules_path = Path(BASE_DIR) / "src" / "api" / api_version / "modules"
+
+    if not modules_path.exists():
+        print(f"Path does not exist: {modules_path}")
+
+    modules = []
+    for module in modules_path.glob("*"):
+        if module.is_dir():
+            module_name = f"src.api.{api_version}.modules.{module.name}"
+            print(f"INITIALIZING MODULE: {module_name}")
+            modules.append(module_name)
+
+    return modules
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -18,8 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    # "apps.analytics",
-]
+] + get_modules_for_version(os.getenv("API_VERSION"))
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -50,8 +73,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "core.wsgi.application"
-
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 
 DATABASES = {
     "default": {
